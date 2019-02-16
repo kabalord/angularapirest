d9c5f6d54b44dff65c6efe61922cf93c57f06ef9
walter roa serrano <walterroaserrano@Admins-MacBook-Pro.local>
2019-02-17 00:45:11 +0100

    como hacer la creacion de rutas

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-index.php
+<?php
+require_once 'vendor/autoload.php';
+
+$app = new\Slim\Slim();
+
+$app->get("/primeraruta", function() use($app){
+	echo "hola mundo desde la primera ruta";
+});
+
+$app->get("/segundaruta", function() use($app){
+	echo "hola mundo desde la primera ruta";
+});
+
+$app->run();
