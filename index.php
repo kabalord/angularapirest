2b93612faada04c0bf3b8c01a003b9ce4a240a56
walter roa serrano <walterroaserrano@Admins-MacBook-Pro.local>
2019-02-17 01:27:53 +0100

    conexion a base de datos, creacion query para insercion de productos a la base de datos y configuracion respuesta servidor

--- index.php
+++ index.php
@@ -3,12 +3,63 @@ require_once 'vendor/autoload.php';
 
 $app = new\Slim\Slim();
 
-$app->get("/primeraruta", function() use($app){
+$db = new mysqli('localhost', 'root', 'root', 'angularapirest');
+
+$app->get("/primeraruta", function() use($app, $db){
 	echo "hola mundo desde la primera ruta";
+	var_dump($db);
 });
 
 $app->get("/segundaruta", function() use($app){
 	echo "hola mundo desde la segunda ruta";
 });
+
+$app->post('/productos', function() use($app, $db){
+	$json = $app->request->post('json');
+	$data = json_decode($json, true);
+
+		if(!isset($data['nombre'])){
+		$data['nombre']=null;
+	}
+
+	if(!isset($data['descripcion'])){
+		$data['descripcion']=null;
+	}
+
+	if(!isset($data['precio'])){
+		$data['precio']=null;
+	}
+
+	if(!isset($data['imagen'])){
+		$data['imagen']=null;
+	}
+
+
+	$query = "INSERT INTO productos VALUES(NULL,".
+	"'{$data['nombre']}',".
+	"'{$data['descripcion']}',".
+	"'{$data['precio']}',".
+	"'{$data['imagen']}'".
+	");";
+
+	$insert = $db->query($query);
+
+		$result = array(
+		'status' => 'error',
+		'code' => 404,
+		'message' => 'El producto NO se ha creado correctamente'
+		);
+
+	if($insert){
+		$result = array(
+			'status' => 'success',
+			'code' => 200,
+			'message' => 'El producto creado correctamente'
+			);
+	}
+	
+	echo json_encode($result);
+});
+
 
 $app->run();
