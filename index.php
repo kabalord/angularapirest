453b4a93ad9635a0df1ebace1cf56173d3d0b842
walter roa serrano <walterroaserrano@Admins-MacBook-Pro.local>
2019-02-19 00:30:20 +0100

    configuracionde cabeceras para permitir el acceso CORS y no tener problemas con las peticiones AJAX

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ $app = new\Slim\Slim();
 
 $db = new mysqli('localhost', 'root', 'root', 'angularapirest');
 
+// CONFIGURACION DE CABECERAS PARA PERMITIR ACCESOS CORS
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 $app->get("/primeraruta", function() use($app, $db){
 	echo "hola mundo desde la primera ruta";
 	var_dump($db);
